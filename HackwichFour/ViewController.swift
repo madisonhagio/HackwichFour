069139fae8f2aa9ab960319e9cf5ee111f7b6929
madisonhagio <hagiom@hawaii.edu>
2022-02-15 18:23:35 -1000

    Completed part 10

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTabLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.firstTabLabel.text = "About"
+        
+        
+        
+        
+        
+        
+        
     }
 
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+    }
+    {func changeColorButtonPressed;.backgroundColor = UIColor (green)
+        
+    }
 
 }
-
